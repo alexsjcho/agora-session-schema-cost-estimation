0ceba6a62e4ae2368dfb40d788eea06f13ae5262
Alex <alexsjcho@gmail.com>
2021-09-17 11:54:02 -0700

    got audienceTotalAggregateVideoResolution function to return data successfully

---
+++ main.js
@@ -0,0 +1,7 @@
+import sessionSchema from "./sessionSchema.js";
+
+let res = sessionSchema.audienceTotalAggregateVideoResolution(
+  sessionSchema.sessionUserConfig
+);
+
+console.log(res);
--- sessionSchema.js
+++ sessionSchema.js
@@ -1,7 +1,7 @@
-
-import {hostTotalAggregateVideoResolution} from "./util/aggregateVideoResolution"
+// import { hostTotalAggregateVideoResolution } from "./util/aggregateVideoResolution";
 
 //TODO: convert to Typescript in the future
+//TODO: fix import of totalResolutionForUser()
 
 const sessionConfig = {
   //number
@@ -15,37 +15,90 @@ const sessionConfig = {
   //number
   hostMaxAggregateVideoResolution: 921600,
   //number
-  maxAudienceAggregateVideoResoltuion:2073600
-}
+  maxAudienceAggregateVideoResoltuion: 2073600,
+};
 
 const sessionUserConfig = {
   //number
-  totalHostCount: 2
+  totalHostCount: 4,
   //number
   totalAudienceCount: 0,
   //number
   hostVideoProfile: 720,
-   //string: "premium","standard"
+  //string: "premium","standard"
   audienceStreamingType: null,
   //string: "premium"
-  hostStreamingType: "premium"
-}
+  hostStreamingType: "premium",
+};
 
+const hostsTotalAggregateVideoResolution = (
+  sessionUserConfig = { totalHostCount, hostVideoProfile }
+) => {
+  return totalResolutionForUser(videoProfile, totalUserCount);
+};
 
-const hostsTotalAggregateVideoResolution = function (sessionUserConfig = {totalHostCount, hostVideoProfile}) {
-  return {
-    totalResolutionForUser(videoProfile, totalUserCount)
+const audienceTotalAggregateVideoResolution = (sessionUserConfig) => {
+  return totalResolutionForUser(
+    sessionUserConfig.hostVideoProfile,
+    sessionUserConfig.totalHostCount,
+    false
+  );
+};
+
+const calculateSessionCost = function (
+  sessionConfig,
+  sessionUserConfig,
+  hostsTotalAggregateVideoResolution
+) {
+  let hostCost, audienceCost;
+
+  if ((sessionConfig.mode = "communication")) {
+    hostCost =
+      sessionUserConfig.totalHostCount * hostsTotalAggregateVideoResolution;
+  } else {
+    hostCost =
+      sessionUserConfig.totalHostCount * hostsTotalAggregateVideoResolution;
+    audienceCost =
+      sessionUserConfig.totalAudienceCount *
+      audiencesTotalAggregateVideoResolution;
   }
-}
+};
 
-const audiencesTotalAggregateVideoResolution = function (sessionUserConfig = totalHostCount, hostVideoProfile ){
-  notSubLocalHostLogic = false
-  return {
-    totalResolutionForUser(totalHostCount, hostVideoProfile)
+const totalResolutionForUser = (
+  videoProfile,
+  totalUserCount,
+  notSubLocalHostLogic
+) => {
+  let aggregateHostVideoResolution = 0;
+  if (notSubLocalHostLogic === true) {
+    aggregateHostVideoResolution = totalUserCount - 1;
+  } else {
+    aggregateHostVideoResolution = totalUserCount;
   }
-}
+  switch (videoProfile) {
+    case 120:
+      return 120 * 160 * totalUserCount;
+    case 180:
+      return 180 * 320 * totalUserCount;
+    case 240:
+      return 240 * 320 * totalUserCount;
+    case 360:
+      return 360 * 640 * totalUserCount;
+    case 480:
+      return 480 * 640 * totalUserCount;
+    case 720:
+      return 720 * 1280 * totalUserCount;
+    case 1080:
+      return 1080 * 1920 * totalUserCount;
+    default:
+      return 120 * 160;
+  }
+};
 
-
-
-
-
+export default {
+  sessionConfig,
+  sessionUserConfig,
+  calculateSessionCost,
+  hostsTotalAggregateVideoResolution,
+  audienceTotalAggregateVideoResolution,
+};
